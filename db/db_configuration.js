1fcb71111b7cb08b2d4749d5f0fffb60043f06f3
john <johnferguson1440@yahoo.com>
2020-09-02 11:49:42 -0500

    updated color

--- db/db_configuration.js
+++ db/db_configuration.js
@@ -1,11 +1,11 @@
 // require pg pool
 const {Pool} = require('pg');
 const pool = new Pool({
-    user:process.env.DATA_USER,
-    host:process.env.DATA_HOST, /*verify with command " \conninfo" in psql repl*/
-    database: process.env.DATA_DATABASE,
-    password: process.env.DATA_PASS,
-    port: process.env.DATA_PORT
+    user:process.env.DATA_USER1,
+    host:process.env.DATA_HOST1, /*verify with command " \conninfo" in psql repl*/
+    database: process.env.DATA_DATABASE1,
+    password: process.env.DATA_PASS1,
+    port: process.env.DATA_PORT1
   
 })
  module.exports = pool;
